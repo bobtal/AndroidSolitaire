bb2882abc1a790b45b4bcf61bb77a479ea9a1c40
bobtal <talevski.boban@gmail.com>
2018-09-03 20:11:06 +0200

    Initial commit

---
+++ app/src/main/java/com/gmail/at/boban/talevski/androidsolitaire/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.gmail.at.boban.talevski.androidsolitaire
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/gmail/at/boban/talevski/androidsolitaire/no_android/GameView.kt
@@ -0,0 +1,5 @@
+package com.gmail.at.boban.talevski.androidsolitaire
+
+interface GameView {
+    fun update(model: GameModel = GameModel)
+}
